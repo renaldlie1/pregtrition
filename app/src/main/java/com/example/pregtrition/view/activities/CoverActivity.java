6a9bcdf4ff19f685b4f68d921daff6745402cc3c
renaldlie <74336039+renaldlie@users.noreply.github.com>
2022-12-24 12:50:43 +0800

    update fragment and color logo
    update fragment and color logo

--- app/src/main/java/com/example/pregtrition/view/activities/CoverActivity.java
+++ app/src/main/java/com/example/pregtrition/view/activities/CoverActivity.java
@@ -2,8 +2,11 @@ package com.example.pregtrition.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.ebanx.swipebtn.OnStateChangeListener;
+import com.ebanx.swipebtn.SwipeButton;
 import com.example.pregtrition.R;
 
 public class CoverActivity extends AppCompatActivity {
@@ -13,6 +16,21 @@ public class CoverActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cover);
 
+        SwipeButton swipeButton = (SwipeButton)findViewById(R.id.swipe_btn);
+
+        swipeButton.setOnStateChangeListener(new OnStateChangeListener() {
+
+            @Override
+
+            public void onStateChange(boolean active) {
+
+                Intent intent = new Intent(CoverActivity.this,MainMenuActivity.class);
+
+                startActivity(intent);
+
+            }
+
+        });
 
 
 
