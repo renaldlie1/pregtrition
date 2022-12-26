67134e40c47a96a39e3ab44061953ead4596166c
renaldlie <74336039+renaldlie@users.noreply.github.com>
2022-12-26 11:18:27 +0800

    switching fragments

--- app/src/main/java/com/example/pregtrition/view/activities/MainMenuActivity.java
+++ app/src/main/java/com/example/pregtrition/view/activities/MainMenuActivity.java
@@ -1,14 +1,23 @@
 package com.example.pregtrition.view.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.pregtrition.R;
+import com.example.pregtrition.view.fragments.AnalyticFragment;
+import com.example.pregtrition.view.fragments.ConsultFragment;
+import com.example.pregtrition.view.fragments.LearnMoreFragment;
+import com.example.pregtrition.view.fragments.MainMenuFragment;
+import com.example.pregtrition.view.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -21,10 +30,36 @@ public class MainMenuActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main_menu);
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
-//        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
 
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.nowPlaying,R.id.upcomingFragment).build();
-//        NavigationUI.setupActionBarWithNavController(MainMenuActivity.this , navHostFragment.getNavController(), appBarConfiguration);
-//        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment = new MainMenuFragment();
+                        break;
+                    case R.id.nav_analytics:
+                        selectedFragment = new AnalyticFragment();
+                        break;
+                    case R.id.nav_learnmore:
+                        selectedFragment = new LearnMoreFragment();
+                        break;
+                    case R.id.nav_consult:
+                        selectedFragment = new ConsultFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, selectedFragment).commit();
+
+                return true;
+            }
+        });
+//
+
+
     }
 }
